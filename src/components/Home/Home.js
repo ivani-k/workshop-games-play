46321ea95a7a6cd7eb12e41a0b153999504d35f1
ivani-k <ivaninakrumova@gmail.com>
2022-10-09 19:04:58 +0300

    Rander latest games conditionaly

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -13,25 +13,29 @@ const Home = () => {
         }, []);
 
         return ( <
-                section id = "welcome-world" >
-                <
-                div className = "welcome-message" >
-                <
-                h2 > ALL new games are < /h2> <
-                h3 > Only in GamesPlay < /h3> <
-                /div> <
-                img src = "./images/four_slider_img01.png"
-                alt = "hero" / >
-                <
-                div id = "home-page" >
-                <
-                h1 > Latest Games < /h1> { /* Display div: with information about every game (if any) */ } {
-                    games.map(x => < LatestGame game = { x }
-                        />)} { /* Display paragraph: If there is no games  */ } <
-                        p className = "no-articles" > No games yet < /p> <
-                        /div> <
-                        /section>
-                    );
-                };
+            section id = "welcome-world" >
+            <
+            div className = "welcome-message" >
+            <
+            h2 > ALL new games are < /h2> <
+            h3 > Only in GamesPlay < /h3> <
+            /div> <
+            img src = "./images/four_slider_img01.png"
+            alt = "hero" / >
+            <
+            div id = "home-page" >
+            <
+            h1 > Latest Games < /h1> {
+                games.length > 0 ?
+                    games.map(x => < LatestGame key = { x._id }
+                        game = { x }
+                        />): < p className = "no-articles" > No games yet < /p>
+                    }
 
-                export default Home;
+                <
+                /div> <
+                /section>
+            );
+        };
+
+        export default Home;
